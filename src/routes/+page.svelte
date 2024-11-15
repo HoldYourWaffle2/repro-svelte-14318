8de34254df64a4e377195e7bb285b723c2b59a43
HoldYourWaffle <ravivanrooijen@live.nl>
2024-11-15 21:06:08 +0100

    Dump

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	let value: string | null = $state(null);
+	let upper = $derived(value?.toUpperCase());
+</script>
+
+{value} becomes {upper}
+
+<button onclick={() => value = 'foo'}>Foo</button>
+<button onclick={() => value = 'bar'}>Bar</button>
